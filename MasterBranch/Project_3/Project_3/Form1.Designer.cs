ef2ab7a569506af77ab0920d2e6f077305f6fda8
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-04-04 14:03:12 +0200

    Visual Studio
    GUI, niet zo veel

--- MasterBranch/Project_3/Project_3/Form1.Designer.cs
+++ MasterBranch/Project_3/Project_3/Form1.Designer.cs
@@ -28,21 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Parser = new System.Windows.Forms.OpenFileDialog();
+            this.Parser_Textbox = new System.Windows.Forms.TextBox();
+            this.Parser_Button = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(834, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.parserToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // parserToolStripMenuItem
+            // 
+            this.parserToolStripMenuItem.Name = "parserToolStripMenuItem";
+            this.parserToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.parserToolStripMenuItem.Text = "Parser";
+            this.parserToolStripMenuItem.Click += new System.EventHandler(this.parserToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Parser
+            // 
+            this.Parser.FileName = "Parser";
+            this.Parser.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Parser_Textbox
+            // 
+            this.Parser_Textbox.Location = new System.Drawing.Point(12, 27);
+            this.Parser_Textbox.Multiline = true;
+            this.Parser_Textbox.Name = "Parser_Textbox";
+            this.Parser_Textbox.Size = new System.Drawing.Size(811, 214);
+            this.Parser_Textbox.TabIndex = 1;
+            // 
+            // Parser_Button
+            // 
+            this.Parser_Button.Location = new System.Drawing.Point(13, 246);
+            this.Parser_Button.Name = "Parser_Button";
+            this.Parser_Button.Size = new System.Drawing.Size(75, 23);
+            this.Parser_Button.TabIndex = 2;
+            this.Parser_Button.Text = "Parse!";
+            this.Parser_Button.UseVisualStyleBackColor = true;
+            this.Parser_Button.Click += new System.EventHandler(this.Parser_Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(834, 281);
+            this.Controls.Add(this.Parser_Button);
+            this.Controls.Add(this.Parser_Textbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parserToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog Parser;
+        private System.Windows.Forms.TextBox Parser_Textbox;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button Parser_Button;
     }
 }
 
--- MasterBranch/Project_3/Project_3/Form1.cs
+++ MasterBranch/Project_3/Project_3/Form1.cs
@@ -19,7 +19,35 @@ namespace Project_3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Daaaag teeeest
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void parserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                string file = openFileDialog1.FileName;
+                Parser_Textbox.Text = file ;
+            }
+            }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Parser_Button_Click(object sender, EventArgs e)
+        {
         }
     }
 }
