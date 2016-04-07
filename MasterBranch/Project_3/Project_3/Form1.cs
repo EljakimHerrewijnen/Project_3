f879e27fe802db5f659692e60e079a564ca1a926
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2016-04-07 13:52:20 +0200

    Push 7/4/2016

--- MasterBranch/Project_3/Project_3/Form1.cs
+++ MasterBranch/Project_3/Project_3/Form1.cs
@@ -17,7 +17,15 @@ namespace Project_3
         public Form1()
         {
             InitializeComponent();
+            _Form1 = this;
         }
+        public static Form1 _Form1;
+
+        public void update(string message)
+        {
+            textBox1.Text = message;   }
+
+        public string GetParserTextBox { get { return Parser_Textbox.Text; } }
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -56,80 +64,13 @@ namespace Project_3
 
         private void Parser_Button_Click(object sender, EventArgs e)
         {
-            char[] Characters = new char[Parser_Textbox.Text.Length];
-
-            string Temp = "";
-            string ParsedQuery = "";
-            int Counter = 0;
-            int result = Parser_Textbox.Text.Length;
-            string newString = Parser_Textbox.Text;
-            string TempString1;
-            string TempString2 = "";
-            string TempString3 = "";
-            int TempCounter = 0;
-            bool AwaitingEndColumn = false;
-            bool EndColumnReached = false;
-            string CatagoryTemp = "";
-            string PreviousChar = "";
-            bool Doublepuntkomma = false;
-            bool ValueChecker = false;
-
-            foreach (char c in newString)
-            {
-                TempString1 = newString;
-                Counter++;
-                TempString2 += c;
-
-                if (TempString2.Contains(";") == false && PreviousChar.Contains(";") == true)
-                {
-                    TempString3 += c;
-                    EndColumnReached = true;
-
-                }
-                else if (TempString2.Contains(";") == false && PreviousChar.Contains(";") == false) { TempString3 += c; }
-                else if (TempString2.Contains(";") == true) { TempCounter++; }
-
-                if (EndColumnReached == true) {
-                    if (TempCounter == 7)
-                    {
-                        CatagoryTemp = ""; CatagoryTemp = TempString3; TempCounter = 0; TempString3 = "";
-                        ValueChecker = false; Doublepuntkomma = false;
-                    }
-                    else if (TempCounter == 5 && Doublepuntkomma == true)
-                    {
-                        TempCounter = 0;
-                        ParsedQuery += "Create Table " + CatagoryTemp + "_" + TempString3 + "( \n";
-                        TempString3 = ""; ValueChecker = false; Doublepuntkomma = false;
-                    }
-                    else if (TempCounter == 2)
-                    {
-                        EndColumnReached = false; Doublepuntkomma = true; TempCounter = 0;
-                    }
-                    else if (TempString2.Contains(";") == true && Doublepuntkomma == true)
-                    {
-                        EndColumnReached = false; Doublepuntkomma = false;
-                        ParsedQuery += TempString3 + " varchar(256), \n"; TempString3 = ""; TempCounter = 0;
-                    }
-
-                   // else if (PreviousChar.Contains(";") == true && Doublepuntkomma == true) { Doublepuntkomma = false; ValueChecker = true; ParsedQuery += TempString3 + " varchar(256),\n"; }
-
-                    //     else if (TempString2.Contains(";") == true && ValueChecker == true && TempCounter == 1) { ParsedQuery += TempString3 + " varchar(256),\n"; } 
-                }
-
-                PreviousChar = TempString2;
-                TempString2 = "";
-                textBox1.Text = ParsedQuery;
-            }
+            Parser TheParser = new Parser();
+            TheParser.CreateQuery();
 
         }
 
-        private void Parser_Textbox_TextChanged(object sender, EventArgs e)
-        {
-            //fileName;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
+        public void Parser_Textbox_TextChanged(object sender, EventArgs e){        }
+        private void button1_Click(object sender, EventArgs e){
             WebBrowser secondbrowser = new WebBrowser();
             secondbrowser.Show();
         }
