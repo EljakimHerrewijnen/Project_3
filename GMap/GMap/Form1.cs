855714657a50eb2e606ad8f749767403cd1c8c11
Marco den Hollander <marcobenik@msn.com>
2016-04-12 16:28:35 +0200

    Revert "Add print button (experimental)"
    This reverts commit 2a3dd71142291314bfed95ea1cdbf89ae5ba5323.

--- GMap/GMap/Form1.cs
+++ GMap/GMap/Form1.cs
@@ -79,19 +79,6 @@ namespace GMap
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Rectangle bounds = this.Bounds;
-            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
-            {
-                using (Graphics g = Graphics.FromImage(bitmap))
-                {
-                    g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-                }
-                bitmap.Save("C://test.jpg");
-            }
-        }
     }
 
 }
