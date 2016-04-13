35bc2b6f180d39c29992a398f8723bf9bddb60b6
MarcHarreman <0894389@hr.nl>
2016-04-13 09:43:17 +0200

    extra buttons

--- GMap/GMap/Form1.Designer.cs
+++ GMap/GMap/Form1.Designer.cs
@@ -47,6 +47,9 @@ namespace GMap
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // gmap
@@ -167,46 +170,73 @@ namespace GMap
             // 
             this.button1.Location = new System.Drawing.Point(676, 38);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(288, 64);
+            this.button1.Size = new System.Drawing.Size(288, 36);
             this.button1.TabIndex = 11;
-            this.button1.Text = "Option 1";
+            this.button1.Text = "Diefstal";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(676, 108);
+            this.button2.Location = new System.Drawing.Point(676, 80);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(288, 64);
+            this.button2.Size = new System.Drawing.Size(288, 36);
             this.button2.TabIndex = 12;
-            this.button2.Text = "Option 2";
+            this.button2.Text = "Drugsoverlast";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(676, 178);
+            this.button3.Location = new System.Drawing.Point(676, 122);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(288, 64);
+            this.button3.Size = new System.Drawing.Size(288, 36);
             this.button3.TabIndex = 13;
-            this.button3.Text = "Option 3";
+            this.button3.Text = "Geweld";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(676, 248);
+            this.button4.Location = new System.Drawing.Point(676, 164);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(288, 64);
+            this.button4.Size = new System.Drawing.Size(288, 36);
             this.button4.TabIndex = 14;
-            this.button4.Text = "Option 4";
+            this.button4.Text = "Inbraak";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(676, 318);
+            this.button5.Location = new System.Drawing.Point(676, 206);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(288, 64);
+            this.button5.Size = new System.Drawing.Size(288, 36);
             this.button5.TabIndex = 15;
-            this.button5.Text = "Option 5";
+            this.button5.Text = "Vandalisme";
             this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(676, 248);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(288, 36);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Schoon en heel";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(676, 290);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(288, 36);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Verkeer";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(676, 332);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(288, 36);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "Overig";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -214,6 +244,9 @@ namespace GMap
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(976, 617);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -254,6 +287,9 @@ namespace GMap
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
