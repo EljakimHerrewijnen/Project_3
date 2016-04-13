e7bd53942b330c112e0d7a1170ee02ee24c9e3e0
Marco den Hollander <marcobenik@msn.com>
2016-04-13 15:56:07 +0200

    unfuck 2

--- GMap/GMap/Form1.cs
+++ GMap/GMap/Form1.cs
@@ -18,9 +18,9 @@ using GMap.NET.WindowsForms.ToolTips;
 
 namespace GMap
 {
-    public partial class Form1 : Form
+    public partial class FormOld : Form
     {
-        public Form1()
+        public FormOld()
         {
             InitializeComponent();
         }
@@ -31,9 +31,9 @@ namespace GMap
             gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
             gmap.SetPositionByKeywords("Rotterdam, The Netherlands");
-//            gmap.BoundsOfMap = new RectLatLng(53.91769699, 4.48100567, 10.12345678, 10.12345678);
-//            gmap.MapProvider.Area = new RectLatLng(30.981178, 105.351914, 2.765142, 4.120995);
-// not yet functional          
+            //            gmap.BoundsOfMap = new RectLatLng(53.91769699, 4.48100567, 10.12345678, 10.12345678);
+            //            gmap.MapProvider.Area = new RectLatLng(30.981178, 105.351914, 2.765142, 4.120995);
+            // not yet functional          
         }
 
 
@@ -63,15 +63,9 @@ namespace GMap
 
         private void gmap_Load(object sender, EventArgs e)
         {
-            
+
         }
 
-        private void ZoomScroll_Scroll(object sender, ScrollEventArgs e)
-        {
-            gmap.Zoom = ZoomScroll.Value;
-            string Value = ZoomScroll.Value.ToString();
-            ZoomBox.Text = Value;
-        }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
@@ -85,7 +79,12 @@ namespace GMap
 
         private void XCoordBox_TextChanged(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
         }
     }
 
