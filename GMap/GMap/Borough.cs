c5d645e158e9d649b39a7195b22af9b140a03939
Pbdekeijzer <Pbdekeijzer@hotmail.com>
2016-04-14 10:44:13 +0200

    Some changes
    Commit

---
+++ GMap/GMap/Borough.cs
@@ -0,0 +1,35 @@
+﻿using GMap.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMap
+{
+    class Borough
+    {
+        private string name;
+        public List<PointLatLng> deelgemeente;
+        private bool draw; //True or False hier
+
+        public Borough(string name, List<PointLatLng> coordlist)
+        {
+            this.name = name;
+            deelgemeente = coordlist;
+            draw = false;
+        }
+
+        public bool Draw
+        {
+            get { return draw; }
+            set { draw = value; }
+        }
+    }
+}
+
+
+//Maak een lijst met alle List<PointLatLng> deelgemeenten
+//Geef per deelgemeente de optie True/False of ze weergegeven moeten worden of niet. (Zou kunnen met checkboxen?)
+//foreach(var deelgemeente in List<Borough>> { if (draw == True) {Heatmaps.Polygons.Add(deelgemeente); } }
+
