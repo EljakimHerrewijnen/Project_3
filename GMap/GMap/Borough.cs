bd12e28636830018fbd63e3a07945f00c1c5f3ed
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2016-04-15 14:59:41 +0200

    Added reading coordinates from file (plz no merge conflicts)

--- GMap/GMap/Borough.cs
+++ GMap/GMap/Borough.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
 using GMap.NET.WindowsForms.ToolTips;
+using System.IO;
 
 namespace GMap
 {
     class Borough
     {
         private string name;
-        public List<PointLatLng> deelgemeente;
+        public List<PointLatLng> deelgemeente = new List<PointLatLng>();
         public List<Area> has_areas;
         private bool draw; //True or False hier
 
-        public Borough(string name, List<PointLatLng> coordlist, bool true_false)
+        public Borough(string name, bool true_false)
         {
             this.name = name;
-            deelgemeente = coordlist;
             draw = true_false;
         }
 
@@ -31,6 +31,64 @@ namespace GMap
             get { return draw; }
             set { draw = value; }
         }
+
+        public void ReadCoords(string path, string naam)
+        {
+            string line;
+            int endname;
+            int endfirstcoord;
+            int i;
+
+            string name;
+            string coord1;
+            string coord2;
+            double north, east;
+
+
+
+            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            using (StreamReader reader = new StreamReader(fileStream, Encoding.ASCII))
+            {
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (line != "")
+                    {
+                        i = 0;
+                        while (line[i] != ' ')
+                        {
+                            i++;
+                        }
+                        endname = i;
+                        name = line.Substring(0, i);
+                        i++;
+                        if (naam == name)
+                        {
+                            while (line[i] != ',')
+                            {
+                                i++;
+                            }
+                            endfirstcoord = i;
+                            coord1 = line.Substring(endname + 1, i - 1 - (endname));
+                            coord2 = line.Substring(i + 2, line.Length - (i + 2));
+
+                            north = double.Parse(coord1);
+                            east = double.Parse(coord2);
+
+
+
+                            deelgemeente.Add(new PointLatLng(north, east));
+                        }
+                    }
+                }
+
+
+
+
+
+            }
+        }
+
     }
 }
 
