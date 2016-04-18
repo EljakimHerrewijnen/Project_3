6c47894d35eeb0f78dd06be7659d18e4cf849676
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2016-04-18 13:11:55 +0200

    Fixed parser errors caused by system limitations regarding multiple consequtive queries

--- MasterBranch/Project_3/Applicatie/Parser v2/Parser v2/DataBaseConnection.cs
+++ MasterBranch/Project_3/Applicatie/Parser v2/Parser v2/DataBaseConnection.cs
@@ -47,6 +47,10 @@ namespace Parser_v2
             string ServerUname = ParserGUI._ParserGUI.Get_ServerUname;
             string ServerPassword = ParserGUI._ParserGUI.Get_ServerUpass;
             string Output_TB = ParserGUI._ParserGUI.Get_Output;
+            int indexchar = 2;
+            int lastindex = 0;
+            string query = "";
+            int ignorecolons = 0;
             try
             {
                 // PostgeSQL-style connection string
@@ -57,13 +61,49 @@ namespace Parser_v2
                 // Making connection with Npgsql provider
                 NpgsqlConnection conn = new NpgsqlConnection(connstring);
                 conn.Open();
-                if (conn.State == ConnectionState.Open) { MessageBox.Show("Connection Open!"); ParserGUI._ParserGUI.update_LB_Server("Connected to database"); }
-                else { MessageBox.Show("Connection not open!"); ParserGUI._ParserGUI.update_LB_Server("Not connected"); }
+                if (conn.State == ConnectionState.Open) {
+                    MessageBox.Show("Connection Open!");
+                    ParserGUI._ParserGUI.update_LB_Server("Connected to database");
+                }
+                else {
+                    MessageBox.Show("Connection not open!");
+                    ParserGUI._ParserGUI.update_LB_Server("Not connected");
+                }
 
                 //Starting to send the Query
-                NpgsqlCommand command = new NpgsqlCommand(Output_TB, conn);
-                command.ExecuteNonQuery();
-                MessageBox.Show("sended");
+
+                //zie hier uitleg waarom er geknipt moet worden http://stackoverflow.com/questions/32886171/npgsql-i-get-42p01-relation-sometable-does-not-exist
+                //we knipppen hier elke command weer uit (grom grom)
+
+                while (indexchar < Output_TB.Length)
+                {
+                    while (Output_TB[indexchar] != ';' && indexchar < Output_TB.Length-1)
+                    {
+                        if(Output_TB[indexchar] == 'D')
+                        {
+                            if(Output_TB[indexchar+1] == 'O')
+                            {
+                                ignorecolons = 2;
+                            }
+                        }
+                        indexchar++;
+                    }
+
+                    indexchar++;
+                    if (ignorecolons == 0 && indexchar < Output_TB.Length)
+                    {
+                        query = Output_TB.Substring(lastindex, indexchar - lastindex);
+                        new NpgsqlCommand(query, conn).ExecuteNonQuery();
+                        lastindex = indexchar;
+                    }
+                    else
+                    {
+                        ignorecolons--;
+                    }
+                }
+
+
+                MessageBox.Show("send");
 
 
             }
