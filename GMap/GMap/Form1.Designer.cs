1e8e56c38c7b123e378a77674b10954368cf2e59
Pbdekeijzer <Pbdekeijzer@hotmail.com>
2016-04-20 09:48:19 +0200

    Commit

--- GMap/GMap/Form1.Designer.cs
+++ GMap/GMap/Form1.Designer.cs
@@ -42,9 +42,9 @@ namespace GMap
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label4 = new System.Windows.Forms.Label();
@@ -69,6 +69,7 @@ namespace GMap
             this.LB_ServerConnection = new System.Windows.Forms.Label();
             this.Btn_AddServer = new System.Windows.Forms.Button();
             this.manual = new System.Windows.Forms.CheckBox();
+            this.manualdeel = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -374,17 +375,17 @@ namespace GMap
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(668, 404);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.Legend = "Legend1";
-            series1.Name = "Area Info";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Area Info";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(323, 203);
             this.chart1.TabIndex = 41;
             this.chart1.Text = "Area Info";
@@ -440,12 +441,24 @@ namespace GMap
             this.manual.UseVisualStyleBackColor = true;
             this.manual.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
+            // manualdeel
+            // 
+            this.manualdeel.AutoSize = true;
+            this.manualdeel.Location = new System.Drawing.Point(815, 245);
+            this.manualdeel.Name = "manualdeel";
+            this.manualdeel.Size = new System.Drawing.Size(108, 17);
+            this.manualdeel.TabIndex = 50;
+            this.manualdeel.Text = "Manual boroughs";
+            this.manualdeel.UseVisualStyleBackColor = true;
+            this.manualdeel.CheckedChanged += new System.EventHandler(this.manualdeel_CheckedChanged);
+            // 
             // FormOld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(1000, 647);
+            this.Controls.Add(this.manualdeel);
             this.Controls.Add(this.manual);
             this.Controls.Add(this.Deel);
             this.Controls.Add(this.Btn_AddServer);
@@ -507,5 +520,6 @@ namespace GMap
         private Label LB_ServerConnection;
         private Button Btn_AddServer;
         private CheckBox manual;
+        private CheckBox manualdeel;
     }
 }
